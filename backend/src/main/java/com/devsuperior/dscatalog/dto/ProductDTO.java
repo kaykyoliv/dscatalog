bd5ec6ba744b351f241814fcb6753ba41ecd45ea
Kayky Oliveira <kaykycf1245@gmail.com>
2024-07-11 16:54:29 -0300

    Product repository, service, resource, GET ok

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -1,5 +1,69 @@
 package com.devsuperior.dscatalog.dto;
 
-public class ProductDTO {
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+public class ProductDTO {
+	
+    private Long id;
+    private String name;
+    private Instant date;
+    private String description;
+    private Double price;
+    private String imgUrl;
+    
+    private List<CategoryDTO> list = new ArrayList<>();
+
+	public ProductDTO(Long id, String name, Instant date, String description, Double price, String imgUrl) {
+		this.id = id;
+		this.name = name;
+		this.date = date;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
+	
+	public ProductDTO(Product entity) {
+		id = entity.getId();
+		name = entity.getName();
+		date = entity.getDate();
+		description = entity.getDescription();
+		price = entity.getPrice();
+		imgUrl = entity.getImgUrl();
+		for(Category cat : entity.getCategories()) {
+			list.add(new CategoryDTO(cat));
+		}
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public List<CategoryDTO> getList() {
+		return list;
+	}  
 }
--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -93,4 +94,23 @@ public class Product {
     public Set<Category> getCategories() {
         return categories;
     }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)  
+			return true;
+		if (obj == null) 
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+    
+    
 }
