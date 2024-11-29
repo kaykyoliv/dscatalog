1c4ffd7a6a0ed68692220ccdb202674a54ba6a14
Kayky Oliveira <kaykycf1245@gmail.com>
2024-11-28 23:00:21 -0300

    generic solution for unordered to ordered

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -107,7 +107,7 @@ public class ProductService {
 		List<Long> productIds = page.map(x -> x.getId()).toList();
 
 		List<Product> entities = repository.searchProductsWithCategories(productIds);
-		entities = Utils.replace(page.getContent(), entities);
+		entities = (List<Product>) Utils.replace(page.getContent(), entities);
 
 		List<ProductDTO> dtos = entities.stream().map(p -> new ProductDTO(p, p.getCategories())).toList();
 
